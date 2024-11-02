11c0cb5961f027770f38bd2ae051c3c66361f202
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-03 01:44:22 +0600

    Coffee Book React Router Project Repository Added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h2 className='text-4xl'>Vite + React</h2>
+      <button className='btn btn-primary'>Click Me</button>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+
+const MainLayout = () => {
+    return (
+      <div>
+        {/* Navbar */}
+        <Navbar></Navbar>
+        <div className="min-h-[calc(100vh-232px)] py-12">
+          {/* Dynamic Sections */}
+          <Outlet></Outlet>
+        </div>
+        {/* Footer */}
+        <Footer></Footer>
+      </div>
+    );
+};
+
+export default MainLayout;
